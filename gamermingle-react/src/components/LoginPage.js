fc9586001f690b169fe4fc611802e15ba750bf24
Richard Marmito <richardmarmito@gmail.com>
2023-12-02 16:03:21 -0800

    Updated LoginPage for Backend, added token and fixed Django urls path

--- gamermingle-react/src/components/LoginPage.js
+++ gamermingle-react/src/components/LoginPage.js
@@ -1,7 +1,47 @@
 import { Form } from "react-bootstrap";
 import "./styles/webStyles.css";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8000/api/token/", {
+        username: username,
+        password: password,
+      });
+      // Handle response data (e.g., save the token)
+      console.log(response.data);
+      localStorage.setItem("token", response.data.access);
+
+      navigate("/profile");
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        setErrorMessage("Failed to log in. Please check your credentials.");
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+    }
+  };
+
   const loginContainerStyles = {
     display: "flex",
     justifyContent: "center",
@@ -38,18 +78,37 @@ function LoginForm() {
                 Hop On.
               </p>
             </strong>
+            {errorMessage && (
+              <div className="alert alert-danger">{errorMessage}</div>
+            )}
+
             <Form id="signup-form">
               <Form.Group controlId="username" className="mb-3">
-                <Form.Label></Form.Label>
-                <Form.Control type="username" placeholder="username" required />
+                <Form.Control
+                  type="username"
+                  placeholder="username"
+                  required
+                  onChange={handleUsernameChange}
+                  value={username}
+                />
               </Form.Group>
               <Form.Group controlId="password">
-                <Form.Control type="password" placeholder="password" required />
+                <Form.Control
+                  type="password"
+                  placeholder="password"
+                  required
+                  onChange={handlePasswordChange}
+                  value={password}
+                />
               </Form.Group>
             </Form>
           </div>
           <div className="button-signup-box justify-content-center d-flex pt-3">
-            <button className="btn loginpage-btn btn-primary btn-lg">
+            <button
+              type="submit"
+              className="btn loginpage-btn btn-primary btn-lg"
+              onClick={handleSubmit}
+            >
               <strong>Sign In</strong>
             </button>
           </div>
