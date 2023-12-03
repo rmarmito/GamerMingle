66ff1d7efc8f2433bad522dcc81f80b110fa8fb0
Richard Marmito <richardmarmito@gmail.com>
2023-12-02 17:37:23 -0800

    Fixed token logic. Fixed Django installed apps implementation

--- gamermingle_backend/gamermingle/urls.py
+++ gamermingle_backend/gamermingle/urls.py
@@ -1,3 +1,5 @@
+# app_name/urls.py
+
 from django.urls import path
 from . import views
 
--- gamermingle_backend/gamermingle_backend/settings.py
+++ gamermingle_backend/gamermingle_backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework_simplejwt.token_blacklist',
+
     'gamermingle',
     'corsheaders',
     'rest_framework',
